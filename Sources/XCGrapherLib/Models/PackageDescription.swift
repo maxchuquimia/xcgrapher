3a81933cee836be9a5e15545f10df6454f67e912
Roger Oba <rogerluan.oba@gmail.com>
2021-08-30 23:48:22 -0300

    Attempt to fix issue with Target's absolute path.

--- Sources/XCGrapherLib/Models/PackageDescription.swift
+++ Sources/XCGrapherLib/Models/PackageDescription.swift
@@ -18,8 +18,13 @@ struct PackageDescription: Decodable {
         path = try values.decode(String.self, forKey: .path)
         // Map all target-related paths to be absolute.
         targets = try values.decode([Target].self, forKey: .targets).map { [path] target -> Target in
+            #if swift(>=5.4)
             let path = path.appendingPathComponent(target.path)
             let sources = target.sources.map { path.appendingPathComponent($0) }
+            #else
+            let path = target.path
+            let sources = target.sources.map { target.path.appendingPathComponent($0) }
+            #endif
             return Target(
                 name: target.name,
                 path: path,
